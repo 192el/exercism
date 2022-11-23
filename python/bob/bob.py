53461ea5b0b19366a6a0ba88c41215638b6c248c
192el <samuelmagoncalves@gmail.com>
2022-11-23 16:42:42 -0300

    pq?

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,12 +1,14 @@
 def response(hey_bob):
-    hey_bob.replace(" ", "")
-    if hey_bob == hey_bob.upper() and hey_bob.endswith("?") and hey_bob.upper() != hey_bob.lower():
+    letterlower = set('abcdefghijklmnopqrstuvwxyz?')
+    letterupper = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ?')
+    numbers = set('1234567890')
+    if hey_bob == hey_bob.upper() and "?" in list(hey_bob) and hey_bob.upper() != hey_bob.lower():
         return "Calm down, I know what I'm doing!"
-    elif hey_bob.endswith("?"):
+    elif hey_bob.strip().endswith("?"):
         return "Sure."
     elif hey_bob == hey_bob.upper() and not hey_bob.endswith("?") and hey_bob.upper() != hey_bob.lower():
         return "Whoa, chill out!"
-    elif hey_bob == "":
+    elif set(hey_bob).isdisjoint(letterlower) and set(hey_bob).isdisjoint(letterupper) and set(hey_bob).isdisjoint(numbers):
         return "Fine. Be that way!"
     else:
         return "Whatever."
