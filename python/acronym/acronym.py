8fcde9d0192aa6e66085b2d6d4e73bbde2b55c17
192el <samuelmagoncalves@gmail.com>
2022-11-27 15:32:35 -0300

    pq?

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,10 +1,11 @@
 def abbreviate(words):
-    words.replace('-', ' ')
-    words.replace('_', ' ')
+    words = words.replace('-', ' ')
+    words = words.replace('_', '')
     letters = list(words)
     acronym = [letters[0]]
     for i in range(len(letters)):
         if letters[i] == ' ':
             acronym.append(letters[i+1])
     result = ''.join(acronym)
+    result = result.replace(' ', '')
     return result.upper()
